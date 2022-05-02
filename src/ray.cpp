32487f62499769592f9e4d91b5df865cc407209e
Sami Batuhan Basmaz Ölmez <repelliuss@gmail.com>
2022-05-02 17:24:43 +0300

    refactor ray

--- src/ray.cpp
+++ src/ray.cpp
@@ -7,40 +7,18 @@
 
 namespace ray {
 
-constexpr f32 shadow_epsilon = 0.000001;
-constexpr f32 intersect_epsilon = 1e-2;
+struct HitData {
+  Point3 pos;
+  Material *material;
+  V3 normal;
+  V3 wo;
+};
 
-template <class T> constexpr T safe_div_or_0(T num, f32 denom) {
-  if (denom != 0.0f) {
-    return num * (1.0f / denom);
-  }
-
-  assert(false && "no safe division");
-
-  return num * 0;
-}
-
-inline V3 barycentric(const Point3 &p, const TriangleFace &tri) {
-  V3 ab = tri.b - tri.a;
-  V3 ac = tri.c - tri.a;
-  V3 ap = p - tri.a;
-
-  f32 d_b_b = dot(ab, ab);
-  f32 d_b_c = dot(ab, ac);
-  f32 d_c_c = dot(ac, ac);
-  f32 d_p_b = dot(ap, ab);
-  f32 d_p_c = dot(ap, ac);
-
-  f32 inv_denom = 1.0f / (d_b_b * d_c_c - d_b_c * d_b_c);
-
-  V3 result;
-
-  result.v = (d_c_c * d_p_b - d_b_c * d_p_c) * inv_denom;
-  result.w = (d_b_b * d_p_c - d_b_c * d_p_b) * inv_denom;
-  result.u = 1 - result.v - result.w;
-
-  return result;
-}
+namespace constant {
+constexpr f32 shadow_epsilon = 1e-4;
+constexpr f32 intersect_epsilon = 1e-4;
+constexpr f32 max_float = std::numeric_limits<f32>::max();
+} // namespace constant
 
 constexpr f32 determinant(const V3 &col0, const V3 &col1, const V3 &col2) {
   f32 term1 = col0.x * (col1.y * col2.z - col2.y * col1.z);
@@ -59,23 +37,18 @@ constexpr f32 intersects_at(const Ray &ray, const TriangleFace &tri) {
 
   f32 A = determinant(ba, ca, ray.direction);
   if (A == 0.0f)
-    return std::numeric_limits<f32>::max();
+    return constant::max_float;
 
   f32 beta = determinant(oa, ca, ray.direction) / A;
   f32 gamma = determinant(ba, oa, ray.direction) / A;
 
   if (beta + gamma <= 1 && 0 <= beta && 0 <= gamma) {
     f32 t = determinant(ba, ca, oa) / A;
-    if (t > intersect_epsilon)
+    if (t > constant::intersect_epsilon)
       return t;
   }
 
-  return std::numeric_limits<f32>::max();
-}
-
-// REVIEW: no inversely square drop?
-constexpr Color ambient(const AmbientLight &light, const Material *mat) {
-  return light.color * mat->ambient;
+  return constant::max_float;
 }
 
 constexpr f32 max(f32 a, f32 b) { return a > b ? a : b; }
@@ -86,36 +59,23 @@ constexpr V3 clamp_max(V3 v, f32 max) {
   return v3(clamp_max(v.x, max), clamp_max(v.y, max), clamp_max(v.z, max));
 }
 
-struct LightHitSpec {
-  const Material *mat;
-  const V3 *normal;
-  const V3 *wo;
-  const V3 *wi;
-};
-
-constexpr Color diffuse(const LightHitSpec &in) {
-  const Material *mat = in.mat;
-  const V3 &normal = *in.normal;
-  const V3 &wi = *in.wi;
-
-  V3 val = mat->diffuse * max(0, dot(wi, normal));
-  return val;
+constexpr Color diffuse(const HitData &hit, const V3 &wi) {
+  return hit.material->diffuse * max(0, dot(wi, hit.normal));
 }
 
-constexpr Color specular(const LightHitSpec &in) {
-  const Material *mat = in.mat;
-  const V3 &normal = *in.normal;
-  const V3 &wo = *in.wo;
-  const V3 &wi = *in.wi;
+constexpr Color ambient(const AmbientLight &light, const Material *mat) {
+  return light.color * mat->ambient;
+}
 
-  V3 h = norm(wo + wi);
-
-  return mat->specular * pow(max(0, dot(normal, h)), mat->phong);
+constexpr Color specular(const HitData &hit, const V3 &wi) {
+  V3 h = norm(hit.wo + wi);
+  return hit.material->specular *
+         pow(max(0, dot(hit.normal, h)), hit.material->phong);
 }
 
 constexpr int in_shadow(const Ray &shadow_ray, f32 light_dist,
                         const Scene &scene) {
-  f32 t_max = std::numeric_limits<f32>::max();
+  f32 t_max = constant::max_float;
 
   for (u32 i = 0; i < scene.mesh_count; ++i) {
     const Mesh *cur_mesh = &scene.meshes[i];
@@ -136,17 +96,11 @@ constexpr int in_shadow(const Ray &shadow_ray, f32 light_dist,
   return 0;
 }
 
-struct HitData {
-  const Point3 pos;
-  const Material *material;
-  const V3 normal;
-  const V3 wo;
-};
-
-inline Color hit_color(std::vector<HitData> &hits, const Scene &scene) {
+constexpr Color hit_color(const HitData *hits, u32 hits_size,
+                          const Scene &scene) {
   Color next_color = v3(0, 0, 0);
 
-  for(i32 hi = hits.size() - 1; hi >= 0; --hi) {
+  for (i32 hi = hits_size - 1; hi >= 0; --hi) {
     Color cur_color = v3(0, 0, 0);
     const HitData &hit = hits[hi];
 
@@ -159,15 +113,9 @@ inline Color hit_color(std::vector<HitData> &hits, const Scene &scene) {
       const V3 wi = light.pos - hit.pos;
       const f32 light_dist = length(wi);
       const V3 norm_wi = norm(wi);
-      const LightHitSpec spec = {
-          .mat = hit.material,
-          .normal = &hit.normal,
-          .wo = &hit.wo,
-          .wi = &norm_wi,
-      };
 
       const Ray shadow_ray = {
-          .origin = hit.pos + norm_wi * shadow_epsilon,
+          .origin = hit.pos + norm_wi * constant::shadow_epsilon,
           .direction = norm_wi,
       };
 
@@ -175,9 +123,10 @@ inline Color hit_color(std::vector<HitData> &hits, const Scene &scene) {
         continue;
 
       const V3 irradiance =
-          safe_div_or_0(light.intensity, light_dist * light_dist);
+          light.intensity * (1.0f / (light_dist * light_dist));
 
-      cur_color += (diffuse(spec) + specular(spec)) * irradiance;
+      cur_color +=
+          (diffuse(hit, norm_wi) + specular(hit, norm_wi)) * irradiance;
     }
 
     next_color = cur_color + next_color * hit.material->reflactance;
@@ -196,19 +145,20 @@ void *threaded_trace(void *arg) {
 int trace(std::vector<Color> *colors, Input *in) {
   const Scene &scene = *in->scene;
   const Camera &cam = scene.cam;
-
-  V2u resolution = cam.resolution;
-  // TODO: to input, doesn't depend on anything here
-  Plane near_plane = near_plane_of_cam(cam);
+  const V2u &resolution = cam.resolution;
+  const Plane near_plane = near_plane_of_cam(cam);
+  
   V2u pixel = v2u(0, in->y_range.beg);
 
   for (; pixel.y < in->y_range.end; ++pixel.y) {
     for (pixel.x = 0; pixel.x < resolution.x; ++pixel.x) {
-      std::vector<HitData> hits;
+
+      HitData hits[scene.max_ray_trace_depth + 1];
+      u32 hits_size = 0;
       Ray ray = ray_between(cam.pos, pixel_on_plane(pixel, near_plane));
 
       for (u32 depth = 0; depth <= scene.max_ray_trace_depth; ++depth) {
-        f32 t_min = std::numeric_limits<f32>::max();
+        f32 t_min = constant::max_float;
         const Mesh *hit = nullptr;
         V3 hit_normal;
 
@@ -230,32 +180,24 @@ int trace(std::vector<Color> *colors, Input *in) {
         if (!hit)
           break;
 
-        HitData hit_data = {
-            .pos = ray.at(t_min),
-            .material = hit->material,
-            .normal = norm(hit_normal),
-            .wo = norm(-ray.direction),
-        };
-        hits.push_back(hit_data);
+        hits[depth].pos = ray.at(t_min);
+        hits[depth].material = hit->material;
+        hits[depth].normal = norm(hit_normal);
+        hits[depth].wo = norm(-ray.direction);
+        ++hits_size;
 
-        if (length(hit_data.material->reflactance) <= 0)
+        if (length(hits[depth].material->reflactance) <= constant::shadow_epsilon)
           break;
 
         ray.direction =
-            2 * dot(hit_data.wo, hit_data.normal) * hit_data.normal -
-            hit_data.wo;
-        ray.origin = hit_data.pos + ray.direction * intersect_epsilon;
+            2 * dot(hits[depth].wo, hits[depth].normal) * hits[depth].normal -
+            hits[depth].wo;
+        ray.origin =
+            hits[depth].pos + ray.direction * constant::intersect_epsilon;
       }
 
-      if (!hits.empty()) {
-        // light calculations
-        Color color = v3(0, 0, 0);
-
-        color += hit_color(hits, scene);
-
-        color = clamp_max(color, 255);
-
-        colors->push_back(color);
+      if (hits_size > 0) {
+        colors->push_back(clamp_max(hit_color(hits, hits_size, scene), 255));
       } else {
         colors->push_back(scene.bg_color);
       }
